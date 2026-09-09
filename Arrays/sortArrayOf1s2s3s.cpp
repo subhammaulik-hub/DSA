bdf624831089c5010242ed5e680aee35fb985cff
Subham Maulik <subhammaulik25@gmail.com>
2026-09-09 12:58:40 +0530

    Added DSA solutions

---
+++ Arrays/sortArrayOf1s2s3s.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sortArray(vector<int>&arr,int n){
+    int low=0, mid=0, high=n-1;
+    while(mid<=high){
+        if(arr[mid]==0){
+            swap(arr[low],arr[mid]);
+            low++;
+            mid++;
+        }
+        else if(arr[mid]==1){
+            mid++;
+        }
+        else{
+            swap(arr[mid],arr[high]);
+            high--;
+        }
+    }
+}
+int main(){
+    int n;
+    cout<<"Enter the size of array: ";
+    cin>>n;
+    vector<int>arr(n);
+    cout<<"Enter elements of array (only 0s, 1s and 2s): ";
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    sortArray(arr,n);
+    cout<<"Sorted array: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
